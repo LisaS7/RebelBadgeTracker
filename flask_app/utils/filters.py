7c89213adcd8ba6ca44ce82b0f04c98946b24433
Lisa <84908214+LisaS7@users.noreply.github.com>
2024-08-08 22:15:13 +0100

    Minor refactor

--- flask_app/utils/filters.py
+++ flask_app/utils/filters.py
@@ -3,7 +3,8 @@ def percentage_format(number):
 
 
 def lighten_colour(hex):
+    FACTOR = 1.1
     h = hex.lstrip("#")
     rgb = tuple(int(h[i : i + 2], 16) for i in (0, 2, 4))
-    rgb_light = tuple(i * 1.5 for i in rgb)
+    rgb_light = tuple(i * FACTOR for i in rgb)
     return f"rgb{rgb_light}"
