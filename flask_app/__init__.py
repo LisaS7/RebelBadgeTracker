e6cdbbd6f286917458af3faf245c90d5bdb0486f
Lisa <84908214+LisaS7@users.noreply.github.com>
2024-08-08 20:20:47 +0100

    Dynamic colours for section heading

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .utils.filters import percentage_format
+from .utils.filters import percentage_format, lighten_colour
 
 
 app = Flask(__name__)
 
-db_path = Path.cwd() / 'database' / 'tracker.db'
+db_path = Path.cwd() / "database" / "tracker.db"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app.jinja_env.filters['percentage_format'] = percentage_format
+app.jinja_env.filters["percentage_format"] = percentage_format
+app.jinja_env.filters["lighter"] = lighten_colour
 
 db = SQLAlchemy(app)
 
--- flask_app/utils/filters.py
+++ flask_app/utils/filters.py
@@ -1,2 +1,9 @@
 def percentage_format(number):
     return f"{number/100:.0%}"
+
+
+def lighten_colour(hex):
+    h = hex.lstrip("#")
+    rgb = tuple(int(h[i : i + 2], 16) for i in (0, 2, 4))
+    rgb_light = tuple(i * 1.5 for i in rgb)
+    return f"rgb{rgb_light}"
