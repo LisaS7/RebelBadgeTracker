335f6788184a18690f3a1d1b6fdcf629db9a9a52
Lisa <84908214+LisaS7@users.noreply.github.com>
2024-08-18 21:34:03 +0100

    Adding vuetify table

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,26 @@
 import "./assets/base.css";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark",
+  },
+});
+
 const app = createApp(App);
 
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
---
+++ frontend/src/views/BadgeList.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps(["badges"]);
+const headers = [
+  { title: "icon", value: "image" },
+  { title: "Name", value: "name", sortable: true },
+  { title: "Progress", value: "progress", sortable: true },
+  { title: "Rating", value: "rating", sortable: true },
+  { title: "Notes", value: "notes" },
+];
+</script>
+
+<template>
+  <v-data-table :headers="headers" :items="props.badges">
+    <template v-slot:item.image="{ item }">
+      <div
+        class="badge-icon"
+        :style="`fill:${item.colour}`"
+        v-html="`${item.image}`"
+      ></div>
+    </template>
+  </v-data-table>
+</template>
+
+<style scoped>
+.table-container {
+  margin: 5rem;
+}
+
+.badge-icon {
+  width: 40px;
+  height: 40px;
+}
+</style>
