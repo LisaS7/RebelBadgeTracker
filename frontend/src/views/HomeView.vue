b41528120584f6988c86c8405a6b46cdc172dc00
Lisa <84908214+LisaS7@users.noreply.github.com>
2024-08-18 16:11:33 +0100

    Add section progress chart

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,16 +1,33 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import BadgeChart from "@/components/Chart.vue";
+import { colorShade } from "@/utils/functions";
 
 const badges = ref([]);
 const loaded = ref(false);
+let patches = {};
+
+function section_colours(data) {
+  for (const patch in data) {
+    patches[patch] = data[patch].map((colour) => {
+      if (colour.substring(0, 4) === "dark") {
+        const colour_hex = colour.substring(5);
+        const darker = colorShade(colour_hex, -150);
+        return darker;
+      } else {
+        return colour;
+      }
+    });
+  }
+}
 
 async function fetchData() {
-  const response = await fetch("http://127.0.0.1:5000/badges");
+  const response = await fetch("http://127.0.0.1:5000/");
   const data_json = await response.json();
-  badges.value = await data_json;
+  const patchData = data_json["patches"];
+  badges.value = data_json["badges"];
   loaded.value = true;
-  console.log("loaded");
+  section_colours(patchData);
 }
 
 onMounted(async () => {
@@ -25,6 +42,18 @@ onMounted(async () => {
     </div>
     <div class="col">
       <h3 class="home-header">Section Patch Progress</h3>
+      <div v-for="(colours, section) in patches" class="row d-flex flex-row">
+        <p class="w-25">{{ section }}</p>
+        <div
+          v-for="colour in colours"
+          class="section-square"
+          :style="
+            colour
+              ? `background: ${colour};`
+              : `background: ${colorShade(colour, -100)};`
+          "
+        ></div>
+      </div>
     </div>
   </div>
   <div class="row">
@@ -46,3 +75,11 @@ onMounted(async () => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.section-square {
+  height: 20px;
+  width: 20px;
+  margin: 10px;
+}
+</style>
