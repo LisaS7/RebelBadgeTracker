a0053ad06a9dcdc69167ef5d1831d215d5768e1b
Lisa <84908214+LisaS7@users.noreply.github.com>
2024-08-18 14:35:47 +0100

    First draft of chart

---
+++ frontend/src/components/Chart.vue
@@ -0,0 +1,50 @@
+<template>
+  <Doughnut :data="data" :options="options" />
+</template>
+
+<script setup>
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  plugins,
+} from "chart.js";
+import { Doughnut } from "vue-chartjs";
+import { colorShade } from "@/utils/functions";
+
+const props = defineProps(["badges"]);
+// props.badges.forEach((badge) => console.log(badge.name));
+
+const labels = props.badges.map((badge) => badge.name);
+const values = props.badges.map((badge) => 1);
+
+const colours = [];
+props.badges.forEach((badge) => {
+  if (badge.complete === true) {
+    colours.push(badge.colour);
+  } else {
+    const darker = colorShade(badge.colour, -150);
+    colours.push(darker);
+  }
+});
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      backgroundColor: colours,
+      data: values,
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  elements: { point: { borderWidth: 0 } },
+  plugins: { legend: { display: false } },
+};
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+</script>
