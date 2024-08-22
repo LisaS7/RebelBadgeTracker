7ba4b897650398f3b3b30950c4c7b59fe0bf1fee
Lisa <84908214+LisaS7@users.noreply.github.com>
2024-08-22 15:38:05 +0100

    Add badges table to section page

--- frontend/src/views/SectionDetails.vue
+++ frontend/src/views/SectionDetails.vue
@@ -1,10 +1,21 @@
 <script setup>
 import { useRoute } from "vue-router";
 import BaseHeading from "@/components/Base/BaseHeading.vue";
+import BadgeTable from "@/components/BadgeTable.vue";
 
 const route = useRoute();
 const section = route.params.name;
+
+const props = defineProps(["badges"]);
+const sectionBadges = props.badges.filter((badge) => badge.section === section);
 </script>
 <template>
   <BaseHeading :text="section" />
+  <BadgeTable :badges="sectionBadges" />
 </template>
+
+<style scoped>
+.v-table {
+  margin-top: 15rem;
+}
+</style>
